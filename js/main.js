f3e6de7a3eab56ac267bc5df86e7b58e814923d6
Carmine Faella <carminefaella5@gmail.com>
2023-02-24 15:21:59 +0100

    Fine lavoro con bonus

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ let cntDom = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++){
 
-    let square = `<div class="square">${i}</div>`;
+    let square = document.createElement('div')
+    square.classList.add('square');
+    cntDom.append(square);
 
-    cntDom.innerHTML += square;
-
-    if(i%3 == 0){
-        square = `<div class="square">Fizz</div>`;
-    } else if(i%5 == 0){
-        square = `<div class="square">Buzz</div>`;
-    }
-};
-
+    if(i%3 == 0 && i%5 == 0){
+        square.append('FizzBuzz');
+        square.style.backgroundColor = 'red';
+    }else if(i%3 == 0){
+        square.append('Fizz');
+        square.style.backgroundColor = 'lightgreen';
+    }else if(i%5 == 0){
+        square.append('Buzz');
+        square.style.backgroundColor = 'orange';
+    }else{
+        square.append(i);
+    };
+}
